a9176d616b4a311ade08cf70a442907b1189430b
Nesma Abouzaid <59185742+NesmaM@users.noreply.github.com>
2023-09-26 11:47:10 -0700

    Part2.java

--- Part2.java
+++ Part2.java
@@ -3,31 +3,73 @@
  * @author (Nesma Abouzaid) 
  * @version (6/20/2023)
  */
-public class Part2 {    
-    public int howMany(String stringa, String stringb){
-        int numOfOcc = 0;
-        int index1 = stringb.indexOf(stringa);
-        if(index1 > -1){
-                numOfOcc++;
-                while (stringb.indexOf(stringa, index1) != -1 && index1 !=-1){
-                       numOfOcc++; 
-                       index1 = stringb.indexOf(stringa, index1+stringa.length());
-                }
-                numOfOcc = numOfOcc -1;
-                } else {
-                    numOfOcc = 0;
-                }
-                return numOfOcc;
-        
+public class Part2 {
+    
+    
+    
+        public String findSimpleGeneModified (String dna, String startCodon, String stopCodon) {
+        //Start codon is "ATG"
+        //Stop Codon is "TAA"        
+        String result = "";
+        if ( Character.isUpperCase(dna.charAt(0)) ){
+            startCodon = startCodon.toUpperCase();
+            stopCodon = stopCodon.toUpperCase();
+            
+        } else {
+            startCodon = startCodon.toLowerCase();
+            stopCodon = stopCodon.toLowerCase();
+            
         }
-             
-
-   
-    public void testing(){
-        System.out.println(howMany("GAA","ATGAACGAATTGAATC"));
-        System.out.println(howMany("AA","ATAAAA"));
         
+        int startIndex = dna.indexOf(startCodon);
+        if (startIndex == -1){ //No ATG
+            return "";
+        }
         
+        int stopIndex = dna.indexOf(stopCodon, startIndex+3);
+        if (stopIndex == -1){ //No TAA
+            return "";
+        } else {
+        
+        result = dna.substring(startIndex, stopIndex+3);
+        if (((stopIndex+3)-startIndex)%3 == 0){
+           return result;
+        }
+    }
+         return "";
     }
 
+    public void testFindGeneSimple(){
+        String gene;
+        //Not multiple of 3
+        String dna = "ATGTCGACCGAAATAA";
+        System.out.println("DNA strand is " + dna);
+        gene = findSimpleGeneModified(dna, "ATG", "TAA");
+        System.out.println("Gene is " + gene);
+        
+        //No ATG
+        dna = "TGTCGACCGAAATAA";
+        System.out.println("DNA strand is " + dna);
+        gene = findSimpleGeneModified(dna, "ATG", "TAA");
+        System.out.println("Gene is " + gene);
+        
+        //No TAA        
+        dna = "ATGTGTCTGAA";
+        System.out.println("DNA strand is " + dna);
+        gene = findSimpleGeneModified(dna, "ATG", "TAA");
+        System.out.println("Gene is " + gene);
+        
+        //Correct
+        dna = "ATGCGATAA";
+        System.out.println("DNA strand is " + dna);
+        gene = findSimpleGeneModified(dna, "ATG", "TAA");
+        System.out.println("Gene is " + gene);
+        
+        dna = "ATGCGCATATAA";
+        System.out.println("DNA strand is " + dna);
+        gene = findSimpleGeneModified(dna, "ATG", "TAA");
+        System.out.println("Gene is " + gene);     
+        
+    }
 }
+
